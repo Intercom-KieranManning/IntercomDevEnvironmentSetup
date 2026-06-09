1a6a28b23fea4901f54c351385cecc79c50c25e3
Kieran Manning <mail@kmanning.ie>
2026-06-09 15:00:29 +0100

    initial

---
+++ setup.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+git clone git@github.com:Intercom-KieranManning/IntercomAPIServer.git APIServer
+git clone git@github.com:Intercom-KieranManning/IntercomClientPython.git ClientPython
+git clone git@github.com:Intercom-KieranManning/IntercomFrontend.git Frontend
